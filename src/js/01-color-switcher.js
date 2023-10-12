6310c1bb5a44aa3b96cb9b272a207fde3753130c
AQR88 <timahaua@gmail.com>
2023-10-12 23:12:40 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,23 +6,23 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-};
+}
 
 startBtn.addEventListener('click', startHandler);
 
 function startHandler() {
-  startBtn.setAttribute('disabled', true);
-  stoptBtn.removeAttribute('disabled');
+  startBtn.setAttribute.disabled = true;
+  stoptBtn.removeAttribute.disabled = true;
   timer = setInterval(() => {
-    body.style.backgroundColor = getRandomHexColor();
-  },1000);
-};
+    const colorChange = body.style.backgroundColor;
+    colorChange = getRandomHexColor();
+  }, 1000);
+}
 
 stoptBtn.addEventListener('click', stopHandler);
 
 function stopHandler() {
-  startBtn.removeAttribute('disabled');
-  stoptBtn.setAttribute('disabled', true);
+  startBtn.removeAttribute.disabled = true;
+  stoptBtn.setAttribute.disabled = true;
   clearInterval(timer);
 }
-
