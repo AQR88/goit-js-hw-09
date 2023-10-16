0117f3d7ac67a8f24e5a72d0dfc825532769f324
AQR88 <timahaua@gmail.com>
2023-10-16 21:51:31 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,23 +1,23 @@
 const startBtn = document.querySelector('button[data-start]');
 const stoptBtn = document.querySelector('button[data-stop]');
 const body = document.querySelector('body');
+let timer = null;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
 
 startBtn.addEventListener('click', startHandler);
 
 function startHandler() {
   startBtn.setAttribute.disabled = true;
   stoptBtn.removeAttribute.disabled = true;
-  timer = setInterval(() => {
-    const colorChange = body.style.backgroundColor;
-    colorChange = getRandomHexColor();
+   timer = setInterval(() => {
+   body.style.backgroundColor = getRandomHexColor();
   }, 1000);
-}
+};
 
 stoptBtn.addEventListener('click', stopHandler);
 
